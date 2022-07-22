76f74c65a387ffd979352523a6bff7da99818b2f
EdgarMTdev <edgarmontiel961@gmail.com>
2022-07-21 19:10:27 -0500

    add: function to carrousel complete

--- src/js/modules/carrousel/index.js
+++ src/js/modules/carrousel/index.js
@@ -1,15 +1,22 @@
 const carrousel = document.getElementById('carrousel')
 const card = document.querySelector('.card')
-console.log(carrousel.clientWidth);
-console.log(card.clientWidth);
+
 const rigth = document.getElementById('rigth')
+const left = document.getElementById('left')
 
 rigth.onclick = () => {
     carrousel.scroll({
         top: 0,
-        left: carrousel.scrollLeft + card.clientWidth+16,
+        left: carrousel.scrollLeft + card.clientWidth + 16,
         behavior: "smooth",
     })
     console.log(carrousel.scrollLeft);
 }
 
+left.onclick = () => {
+    carrousel.scroll({
+        top: 0,
+        left: carrousel.scrollLeft - card.clientWidth - 16,
+        behavior: "smooth"
+    })
+}
